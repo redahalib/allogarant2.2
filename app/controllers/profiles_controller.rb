b08a5d1b3ea30e83e03c10971b58b3253cda8674
Ghita Aachab <ghita.aachab@gmail.com>
2024-05-23 17:05:38 +0100

    fixed cloudinary

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,17 +15,10 @@ class ProfilesController < ApplicationController
   end
 
   def update
-
-    @passport = Passport.new
-    @certificat = Certificat.new
-    @passport.user = @user
-    @passport.photos = params[:user][:passport]
-    @certificat.user = @user
-    @certificat.photos = params[:user][:certificat]
-
     @user = User.find(params[:id])
     if @user == current_user
       if @user.update(user_params)
+        save_documents
         redirect_to edit_profile_path(@user), notice: 'Documents enregistrés avec success'
       else
         render :edit
@@ -36,6 +29,11 @@ class ProfilesController < ApplicationController
   end
 
   private
+
+  def save_documents
+    @passport = @user.passports.create(photos: params[:user][:passport])
+    @certificat = @user.certificats.create(photos: params[:user][:certificat])
+  end
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :phone, :need, :budget,
