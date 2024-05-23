c98c1aaf4371e1b1a16893d846549fce82e29de1
Ghita Aachab <ghita.aachab@gmail.com>
2024-05-23 15:33:12 +0100

    added contacts page and messages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_23_113826) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_23_132651) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -47,6 +47,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_23_113826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_certificats_on_user_id"
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "message"
+    t.string "email"
+    t.string "full_name"
+    t.string "subject"
+    t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "passports", force: :cascade do |t|
