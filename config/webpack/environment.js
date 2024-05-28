ef6024909aaa4b756147ad0b0e3e9fa200d03e4a
Ghita Aachab <ghita.aachab@gmail.com>
2024-05-28 17:29:27 +0100

    added new file slugignore

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,11 @@
 const { environment } = require('@rails/webpacker');
 const webpack = require('webpack');
 
+environment.config.set('output', {
+  path: path.resolve(__dirname, 'app/assets/javascripts'), // Adjust this path as needed
+  filename: 'application.js' // Or your preferred output filename
+});
+
 environment.config.set('node', {
   __dirname: false,
   __filename: false,
