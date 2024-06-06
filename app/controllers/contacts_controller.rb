39516ffe64d2250aa876c338afeaa87f1b01160a
Ghita Aachab <ghita.aachab@gmail.com>
2024-06-06 11:37:39 +0100

    added filters for messages

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -12,9 +12,16 @@ class ContactsController < ApplicationController
 
   def create
     @contact = Contact.new(contact_params)
-    if @contact.save
-      redirect_to root_path, notice: "Contact message sent successfully!"
+
+    if @contact.allowed_msg
+      if @contact.save
+        redirect_to root_path, notice: "Votre message a été envoyé avec succès !"
+      else
+        flash.now[:alert] = "Erreur lors de l'enregistrement du message."
+        render :new
+      end
     else
+      flash.now[:alert] = "Message indésirable."
       render :new
     end
   end
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,13 @@
 class Contact < ApplicationRecord
   validates :email, presence: true
   validates :message, presence: true
+
+  AUTHORIZED_WORDS = %w(étude etude loyer budget neuveu niece nièce étudiant etudiant logement location appartement caution garant residence fils enfant fille pere père mère mere frere frère soeur ville universit université ecole école lycee lycée)
+
+  def allowed_msg
+		rslt = AUTHORIZED_WORDS.map do |word|
+			self.message.split.include?(word)
+		end.uniq
+		allowed_msg = rslt.include?(true)
+	end
 end
